aa6523b02b8694b10620239d56537fa8ae6e141f
jibeom04 <jibeom.0408@gmail.com>
2026-03-20 10:05:22 +0900

    .

--- Baekjoon/Baekjoon.c
+++ Baekjoon/Baekjoon.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 
-int mian()
+int main()
 {
-    printf("Hello World");
+    int n;
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            printf(" ");
+        }
+        for(int j=i*2+1; j>0; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    for(int i=n-1; i>0; i--)
+    {
+        for(int j=i; j<n; j++)
+        {
+            printf(" ");
+        }
+        for(int j=i*2-1; j>0; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 
     return 0;
 }
+/*
+    *
+   ***
+  *****
+ *******
+********
+*/

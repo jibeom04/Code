906ba4324edccff21f88e56aa14ab1d70635fc0e
jibeom04 <jibeom.0408@gmail.com>
2026-04-09 19:37:45 +0900

    .

--- OOP/testProject4/src/testProject4/Test3.java
+++ OOP/testProject4/src/testProject4/Test3.java
@@ -1,31 +1,31 @@
-package testProject4;
-
-public class Test3 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
-
-class Point {
-	int x = 10;
-	int y = 20;
-	Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
-class Point3D extends Point{
-	int z = 30;
-	Point3D() {
-		this(100, 200, 300);
-	}
-	Point3D(int x, int y, int z) {
-		super(x, y);
-		this.z = z;
-	}
-	
-}
+//package testProject4;
+//
+//public class Test3 {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
+//
+//class Point {
+//	int x = 10;
+//	int y = 20;
+//	Point(int x, int y) {
+//		this.x = x;
+//		this.y = y;
+//	}
+//}
+//
+//class Point3D extends Point{
+//	int z = 30;
+//	Point3D() {
+//		this(100, 200, 300);
+//	}
+//	Point3D(int x, int y, int z) {
+//		super(x, y);
+//		this.z = z;
+//	}
+//	
+//}
